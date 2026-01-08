793317fb4b8e1b68cd6c40e16631cf7b3884c3eb
lijianan <djfos@outlook.com>
2026-01-08 17:07:04 +0800

    update test

--- test/orcalab/test_path.py
+++ test/orcalab/test_path.py
@@ -1,48 +1,45 @@
 from orcalab.path import Path
 
-import unittest
+
+def test_default_construct_path_is_root_path():
+    p = Path()
+    assert p == Path.root_path()
 
 
-class TestAddFunction(unittest.TestCase):
-    # def test_empty_path_is_invalid(self):
-    #     with self.assertRaises(Exception):
-    #         p = Path("")
-
-    def test_default_construct_path_is_root_path(self):
-        p = Path()
-        self.assertEqual(p, Path.root_path())
-
-    def test_equal(self):
-        self.assertEqual(Path("/a"), Path("/a"))
-        self.assertEqual(Path("/box1"), Path("/box1"))
-        self.assertEqual(Path(), Path())
-
-    def test_as_key(self):
-        d = {}
-        d[Path("/a")] = 1
-        d[Path("/b")] = 2
-
-        self.assertEqual(Path("/a") in d, True)
-        self.assertEqual(d[Path("/a")], 1)
-        self.assertEqual(d[Path("/b")], 2)
-
-    def test_is_descendant_of(self):
-        self.assertEqual(Path("/a").is_descendant_of(Path("/")), True)
-        self.assertEqual(Path("/a/b").is_descendant_of(Path("/a")), True)
-        self.assertEqual(Path("/a/b/c").is_descendant_of(Path("/a")), True)
-
-        self.assertEqual(Path("/a").is_descendant_of(Path("/a")), False)
-        self.assertEqual(Path("/a/b").is_descendant_of(Path("/b")), False)
-        self.assertEqual(Path("/a/b").is_descendant_of(Path("/c")), False)
-        self.assertEqual(Path("/a/b").is_descendant_of(Path("/a/b/c")), False)
-        self.assertEqual(Path("/aaa").is_descendant_of(Path("/a")), False)
-
-    def test_parent(self):
-        self.assertEqual(Path("/a").parent(), Path("/"))
-        self.assertEqual(Path("/a/b").parent(), Path("/a"))
-        self.assertEqual(Path("/a/b/c").parent(), Path("/a/b"))
-        self.assertEqual(Path("/").parent(), None)
+def test_equal():
+    assert Path("/a") == Path("/a")
+    assert Path("/box1") == Path("/box1")
+    assert Path() == Path()
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_as_key():
+    d = {Path("/a"): 1, Path("/b"): 2}
+    assert Path("/a") in d
+    assert d[Path("/a")] == 1
+    assert d[Path("/b")] == 2
+
+
+def test_is_descendant_of():
+    assert Path("/a").is_descendant_of(Path("/")) is True
+    assert Path("/a/b").is_descendant_of(Path("/a")) is True
+    assert Path("/a/b/c").is_descendant_of(Path("/a")) is True
+
+    assert Path("/a").is_descendant_of(Path("/a")) is False
+    assert Path("/a/b").is_descendant_of(Path("/b")) is False
+    assert Path("/a/b").is_descendant_of(Path("/c")) is False
+    assert Path("/a/b").is_descendant_of(Path("/a/b/c")) is False
+    assert Path("/aaa").is_descendant_of(Path("/a")) is False
+
+
+def test_parent():
+    assert Path("/a").parent() == Path("/")
+    assert Path("/a/b").parent() == Path("/a")
+    assert Path("/a/b/c").parent() == Path("/a/b")
+    assert Path("/").parent() is None
+
+
+def test_is_root():
+    assert Path("/").is_root() is True
+    assert Path().is_root() is True
+    assert Path("/a").is_root() is False
+    assert Path("/a/b").is_root() is False
